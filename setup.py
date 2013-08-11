7d3bf0bea06450df5fe86ce54f86e9a9543c2cf7
sleepingpills <tom.farnbauer@gmail.com>
2013-08-11 16:21:32 +0200

    Separated out zmq header and library folders. Added support for bundled libzmq support. Added version check for gevent.

--- setup.py
+++ setup.py
@@ -1,23 +1,61 @@
 import os
 import zmq
 import sys
+import gevent
 
 from setuptools import setup, Extension
 from Cython.Distutils import build_ext
 
 include_dirs = zmq.get_includes()
-library_dirs = []
+zmq_lib_dir = None
+
+# Check version requirements
+min_gevent_version = (1, 0, 0)
+
+if gevent.version_info < min_gevent_version:
+    print("Error: gevent must be version %s or higher, got %s" % (str(min_gevent_version), str(gevent.version_info)))
+    sys.exit(1)
 
 # Use any extra include and library locations
 for arg in sys.argv[:]:
-    if arg.find("--zmq=") == 0:
-        zmq_dir = arg[6:]
-        include_dirs.append(os.path.join(zmq_dir, "include"))
-        library_dirs.append(os.path.join(zmq_dir, "lib"))
+    if arg.find("--zmq-include=") == 0:
+        include_dir = arg[14:]
+        include_dirs.append(os.path.join(include_dir))
         sys.argv.remove(arg)
-        print "zmq dir:", zmq_dir
+    elif arg.find("--zmq-lib=") == 0:
+        if zmq_lib_dir is not None:
+            print("Error: multiple instances of `zmq-lib` specified")
+            sys.exit(1)
 
-zmq_lib = "libzmq" if sys.platform.startswith("win") else "zmq"
+        zmq_lib_dir = os.path.abspath(os.path.expanduser(arg[10:]))
+        sys.argv.remove(arg)
+
+# Determine if pyzmq contains a bundled libzmq
+if sys.platform.startswith("win"):
+    zmq_lib_name = "libzmq"
+    zmq_lib_ext = ".dll"
+else:
+    zmq_lib_name = "zmq"
+    zmq_lib_ext = ".dylib" if sys.platform == "darwin" else ".so"
+
+pyzmq_dir = os.path.dirname(zmq.__file__)
+zmq_lib_path_bundled = os.path.join(pyzmq_dir, "libzmq" + zmq_lib_ext)
+
+if os.path.exists(zmq_lib_path_bundled):
+    # Warn the user in case libzmq dir is explicitly specified but it is not the same as the bundled libzmq
+    if zmq_lib_dir is not None and pyzmq_dir != zmq_lib_dir:
+        print("Warning: pyzmq is using bundled libzmq, but splice.io will be\n"
+              "         linked against instance in directory: %s" % zmq_lib_dir)
+    else:
+        zmq_lib_dir = pyzmq_dir
+
+# Exit in case there was no explicit libzmq dir specified and it isn't bundled with pyzmq
+if zmq_lib_dir is None:
+    print("Error: libzmq directory was not specified and a pyzmq-bundled version couldn't be found either")
+    sys.exit(1)
+
+
+library_dirs = [zmq_lib_dir] if zmq_lib_dir is not None else []
 
 setup(
     name="splice.io",
@@ -34,7 +72,8 @@ setup(
     ext_modules=[Extension("splice.receiver",
                            ["splice/receiver.pyx"],
                            library_dirs=library_dirs,
-                           libraries=[zmq_lib])],
+                           runtime_library_dirs=library_dirs,
+                           libraries=[zmq_lib_name])],
     include_dirs=include_dirs,
     install_requires=[
         "gevent >= 1.0dev",
